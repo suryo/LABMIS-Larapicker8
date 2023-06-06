e34fd87c8b9c3e17ccd1ddf9863c44b292a22cbb
suryo <suryoatm@gmail.com>
2023-06-06 16:03:12 +0700

    modified mylearning controller

--- app/Http/Controllers/Member/MyLearningController.php
+++ app/Http/Controllers/Member/MyLearningController.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
-use DB;
+use Illuminate\Support\Facades\DB;
 use Hash;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Support\Arr;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
@@ -24,8 +25,21 @@ class MyLearningController extends Controller
 
     public function index(Request $request)
     {
+        //dump(Auth::user()->email);
+        $res_mahasiswa = DB::select('select * from akademik_mhs where email="'.Auth::user()->email.'"');
+        $nip = $res_mahasiswa[0]->nip;
+        //dump($nip);
+
+        $i = 0;
+
+        $res_praktikum = DB::select('select rp.*, p.mk_praktikum, p.deskripsi, jp.hari, jp.jam_mulai, jp.jam_selesai from akademik_registrasi_praktikum as rp 
+        INNER JOIN akademik_praktikum as p on rp.id_praktikum = p.id
+        INNER JOIN akademik_jadwal_praktikum as jp on jp.id_praktikum = rp.id_praktikum 
+        where rp.nip='.$nip);
+        //dd($res_praktikum);
+
         $link = "www.supresso.com/beta";
-        return view('member/mylearning',compact('link'));
+        return view('member/mylearning',compact('res_praktikum','i'));
     }
 
 
