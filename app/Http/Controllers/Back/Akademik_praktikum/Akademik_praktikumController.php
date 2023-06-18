ea57fc5bfb5fca77bfba17a21e3e7af6198f77c8
suryo <suryoatm@gmail.com>
2023-06-18 21:30:18 +0700

    fixed bug Akademik_praktikum

--- app/Http/Controllers/Back/Akademik_praktikum/Akademik_praktikumController.php
+++ app/Http/Controllers/Back/Akademik_praktikum/Akademik_praktikumController.php
@@ -18,7 +18,7 @@
             public function index(Request $request)
             {
                 $data = Akademik_praktikum::orderBy("id","DESC")->get();
-                return view("Back.Akademik_praktikum.index",compact("data"))
+                return view("back.Akademik_praktikum.index",compact("data"))
                     ->with("i", ($request->input("page", 1) - 1) * 5);
             }
         
@@ -30,7 +30,7 @@
         
             public function create()
             {
-                return view("Back.Akademik_praktikum.create");
+                return view("back.Akademik_praktikum.create");
             }
         
         
